91fac95ceb9c0d290ec062a5ced046520de4725f
nash-dessin <n.whinga@gmail.com>
2026-08-23 21:47:46 +0300

    function: add a result data function to dynamically display data

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,5 +1,15 @@
-function Results() {
-  return <h1>Results Page</h1>;
-}
+import { useState } from "react";
+import { ArrowLeft, Check, ChevronDown, RotateCcw, X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+function resultData() {
+  return {
+    quizTitle: {`${localStorage.getItem("quizTitle")}`},
+    score: {parseInt(localStorage.getItem("score"))},
+    totalQuestions: {parseInt(localStorage.getItem("totalQuestions"))},
+    questions: JSON.parse(localStorage.getItem("questions")) || [],
+  }};
+
+
 
 export default Results;
